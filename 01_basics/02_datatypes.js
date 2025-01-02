ca875e7990eb24ff2f0562c8dae806e714b90904
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-01-02 05:05:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  //used to treat all JS code as newer version
+
+//alert(3+3)  // it works on browser but not in nodeJS , it has different syntax to use the same
+
+let name = "Daksh"   //String datatype
+let age = 19 //Num datatype
+let isLoggedIn = true   //Boolean datatype
+
+
+// Datatypes -:
+// 1. String
+// 2. Num
+// 3. boolean 
+// 4. bigint
+// 5. null => Standalone Value
+// 6. undefined
+// 7. Symbol - used in react (uniqueness)
+// 8. object
+
+console.log(typeof undefined)    //typeof undefined
+console.log(typeof null)    //typeof null is object
