328cdc24d8848610d3c2c5196179fba382a56561
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-01-02 05:05:47 +0000

    datatype summary

---
+++ 01_basics/05_dataypeSummary.js
@@ -0,0 +1,31 @@
+// Primitive Datatypes - String , Number , Boolean , null , symbol , undefined , BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n
+
+
+// Reference (Non primitive) - Array , objects , functions
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// Memory // 
+// stack(Primitive) , heap(non primitive)
