16a9ed9e9a2a250207428828194e160bbaf8e9f6
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-01-06 07:12:10 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,4 +1,27 @@
 // Array
-
-let myArr = [1,2,3,4,5]
+const myArr = [1,2,3,4,5]
 console.log(myArr);
+
+let myHeroes = ["Shaktiman, Nagraaj"]
+
+const myArr1 = new Array(2,4,6,8)
+console.log(myArr1);
+
+//Array Methods
+// myArr.push(6)
+// myArr.push(9)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+myArr.unshift(8)            //Used to enter element in starting of array
+console.log(myArr);
+
+
+
+
+
+
+
+
