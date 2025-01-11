460f5e70819c4a87dccefe258da8cf0a8664bde5
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-01-11 04:44:31 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -18,10 +18,29 @@ console.log(myArr1);
 myArr.unshift(8)           //Used to enter element in starting of array
 console.log(myArr);
 
-myArr.shift()      //Used to delete the unshift element
+myArr.shift()      //Used to delete the unshift element, no parameter is given 
 
 
-console.log(myArr.includes(1));
+console.log(myArr.includes(1));    //Returns true/false
+console.log(myArr.indexOf(4));    //Returns -1 , if no index is found 
+
+const newArr = myArr.join()
+console.log(newArr);      //type => String
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)   //removes the given elements between given indexes
+console.log("C ", myArr);
+console.log(myn2);
+
 
 
 
