0ed728205925c1240670194f53999488c20a0e47
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-01-12 15:31:53 +0000

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,41 @@
+const tinderUser = new Object();       //method to declare an object 
+
+tinderUser.id = "123abc"
+tinderUser.name = "Ronny"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Daksh",
+            lastname: "Yadav"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);    //Elements are accesses using . operator
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+//const obj3 = {obj1,obj2}   //not used to combine as combines the whole object
+//const obj3 = Object.assign(obj1,obj2) //not used that much
+
+const obj3 = {...obj1 , ...obj2}
+console.log(obj3);
+
+
+console.log(Object.keys(tinderUser))           // We get an Array in return
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('name'));     // To check that Key exists in Object or not
+
+
+
+
+
+
