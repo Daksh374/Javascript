eb43397a1414185198e763e388a946a8b2ba74cb
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-02-04 08:35:45 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+// if else statement
+const isUserLoggedIn = true;
+const temperature = 43;
+
+if (2 != 3){            //Statement is true so it will be executed
+    console.log("Code executed Successfully");
+    
+}
+
+// if (temperature < 50 ){
+//     console.log("Temperature is less than 50");
+    
+// } else{
+//     console.log("Temperature is greater than 50");
+// }
+
+// const score = 200;
+// if(score > 100){
+//     let power = "You can fly";
+//     console.log(`User Power: ${power}`);
+    
+// }
+
+
