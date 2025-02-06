79cee9d7d1064130df5a805d04ee73c49d73765e
Daksh374 <148604124+Daksh374@users.noreply.github.com>
2025-02-06 15:41:59 +0000

    if else basics

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,4 +21,30 @@ if (2 != 3){            //Statement is true so it will be executed
     
 // }
 
+// const balance = 90;
+// if (balance < 1000) {
+//     console.log("Less than 1000");
 
+// } else if(balance < 500){
+//     console.log("Less than 500");
+
+// } else if(balance < 250){
+//     console.log("Less than 250");
+// } else{
+//     console.log("Greater than 1000");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+
+const LoggedInFromGoogle = false; 
+const LoggedInFromEmail = true;
+
+if (userLoggedIn && debitCard) {
+    console.log("Allowed to purchase");
+}
+
+if(LoggedInFromGoogle || LoggedInFromEmail){
+    console.log("User Logged In");
+    
+}
